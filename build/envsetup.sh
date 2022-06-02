f3850bc106bebaa75f0e98616e50debdd7937987
Ruilin Hao <ruilinhao@asrmicro.com>
2022-06-02 13:53:27 +0800

    Update the envsetup.sh to download the toolchain automatically
    Signed-off-by: Ruilin Hao <ruilinhao@asrmicro.com>

--- build/envsetup.sh
+++ build/envsetup.sh
@@ -2,14 +2,19 @@
 
 export TREMO_SDK_PATH=$(pwd)
 
+GNU_ARM_EMBEDDED_TOOLCHAIN_URL='https://developer.arm.com/-/media/Files/downloads/gnu-rm/9-2020q2/gcc-arm-none-eabi-9-2020-q2-update-win32.zip?revision=95631fd0-0c29-41f4-8d0c-3702650bdd74&hash=60FBF84A2ADC7B1F508C2D625E831E1F1184F509'
+
 which arm-none-eabi-gcc >/dev/null 2>&1
 if [ $? -ne 0 ]; then
     if [ ! -e $TREMO_SDK_PATH/tools/toolchain/bin/arm-none-eabi-gcc.exe ]; then
-    	cd ./tools/toolchain/
-    	unzip gcc-arm-none-eabi-9-2020-q2-update-win32.zip
-    	cd -
+        if [ ! -e $TREMO_SDK_PATH/tools/toolchain/gcc-arm-none-eabi-9-2020-q2-update-win32.zip ]; then
+            wget $GNU_ARM_EMBEDDED_TOOLCHAIN_URL -O $TREMO_SDK_PATH/tools/toolchain/gcc-arm-none-eabi-9-2020-q2-update-win32.zip
+        fi
+        cd ./tools/toolchain/
+        unzip gcc-arm-none-eabi-9-2020-q2-update-win32.zip
+        cd -
     fi
-    
+
     which arm-none-eabi-gcc >/dev/null 2>&1
     if [ $? -ne 0 ]; then
         export PATH="$PATH:$TREMO_SDK_PATH/tools/toolchain/bin/"
